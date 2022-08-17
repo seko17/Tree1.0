570ea9dc5adcc393f0459de7351f52cfdc3dedb2
seko17 <34337236+seko17@users.noreply.github.com>
2022-08-17 17:39:47 +0200

    second commit

--- sketch.js
+++ sketch.js
@@ -1,72 +1,149 @@
+
+// 1) Create a forest with variation (with branch and image(pg) )
+
+// 2) Add "humans" through pictures
+  // IMAGE
+  //   2.1) https://p5js.org/reference/#/p5/image
+  //   2.2) https://p5js.org/reference/#/p5/loadImage
+  //   2.3) https://p5js.org/reference/#/p5/preload
+  //?  2.4) https://p5js.org/reference/#/p5/tint
+
+// 3) Add a structure for displaying trees (and humans?)
+// JSON
+//   3.1) https://p5js.org/reference/#/p5/loadJSON
+//   3.2) Create your own JSON in glitch and use it to hold position information
+
 let angle = .8;
-// let movetree = 0.01; /// each pass offset by 1% of screen width
-// let density = 0; /// make tree pass counter
-// let extrabranch = 0; /// randomly make extra branch
-// let lessbranch = 0; /// not implimented
-// let trunkthick = 0.11; /// each branch diminished size 
-// let limbmin = 10; /// small limbs makes dense tops
-// let treemax = 3; /// how many passes per tree
+let split = 3;          // number of divisions at the end of each branch
+let level = 0;           // global variable to track what is being drawn
 let len = 100/(level + 1);
 let thick = 20/(level+ 1);
-
-let maxLevel = 7;        // number of levels of recursion. more than 10 will throw
-                         // an error because the palette is not large enough
-
-let split = 3;           // number of divisions at the end of each branch
-                         
-let level = 0;           // global variable to track what is being drawn
-
+let maxLevel = 7;        
 // color palettes can be used to set the color of different levels
 let palette = ["#602020","#803030","#C06030","#F09060","#60C060","#60F060","#60B060","#60E060","#60C060","#60D060"];
+let pg;
+let time = 0;
+let particles = [];
+let people = [];
+var spot = {
+  x: 100,
+  y: 50,
+}
 
+function preload(){
+  person = loadImage("assets/person.png")
+}
 
 function setup() {
-  createCanvas(600, 400);
- 
+  createCanvas(windowWidth, windowHeight);
+  pg = createGraphics(width, height);
+  for(let i = 0;i<width/10;i++){
+    particles.push(new Particle());
+  }
+  
 }
 
 function draw() {
   background("#D0E0F0");
-  
+
   fill("#40F040");
   noStroke();
   rect(0,300,width,height);
   
-  translate(300,350);  // starts the trunk in the right location
-  rotate(PI);          // points the tree to the sky!
-  
-  branch()
-  noLoop();
+  image(pg, 0,0 );
+
+  if (millis() >= time + 1000){
+    drawTree();
+    time = millis();
+  }
+
+  for(let i = 0;i<particles.length;i++) {
+    particles[i].draw();
+    particles[i].moveParticle();
+  }
+  rotate(PI);
+  for(let i = 0; i<4;i++){
+    showPeople();
+  }
+ 
  
 }
 
+class Particle {
+  constructor(){
+    this.x = random(0,width);
+    this.y = random(0,height);
+    this.r = random(1,8);
+    this.xSpeed = random(-2,2);
+    this.ySpeed = random(-1,1.5);
+  }
 
-function branch() {
+// creation of a particle.
+  draw() {
+    noStroke();
+    fill('rgba(200,169,169,0.5)');
+    circle(this.x,this.y,this.r);
+  }
+
+// setting the particle in motion.
+  moveParticle() {
+    if(this.x < 0 || this.x > width)
+      this.xSpeed*=-1;
+    if(this.y < 0 || this.y > height)
+      this.ySpeed*=-1;
+    this.x+=this.xSpeed;
+    this.y+=this.ySpeed;
+  }
+
+}
+function showPeople(){
+  spot.x = random (0, width);
+  spot.y = random (0, 100);
+  pg.image(person,500,500)
+}
+
+
+function drawTree() {
+  for (var x = 5; x < 400; x = x+100){
+		for (var y = 5; y < 600; y = y+50){
+      pg.push();
+      pg.translate(x,y);  // starts the trunk in the right location
+      pg.rotate(Math.PI);          // points the tree to the sky!
+      branch()
+    pg.pop();
+		}	
+	}	
+  
+}
+
+function branch() {  
   let angle = .8;              // the angle of the split for sub-branches
   let len = 100/(level+1);      // the length of the branch
   let thick = 20/(level+1);     // the thickness of the branch
   
-  stroke(palette[level]);      
-  strokeWeight(thick);
+  pg.stroke(palette[level]);      
+  pg.strokeWeight(thick);
   
-  push();             // PUSH to a new drawing state. 
-  if(level!=0) rotate(-0.5*(split-1)*angle);   // see comment below
+  pg.push();           
   
-  line(0,0,0,len);                             // the only actual "drawing" code
+  if(level!=0) pg.rotate(-0.5*(split-1)*angle); 
+  
+  pg.line(0,0,0,len);                             // the only actual "drawing" code
 
-  translate(0, len);                           // move to the end of the branch 
+  console.log(random(0,8));
+  pg.translate(random(0,8), len);                           // move to the end of the branch 
   
   if (level<maxLevel-1) {                      // quit when we reach base state
     
     level++;                                   // increase the level 
     for (let i = 0; i<split; i++) {            // draw more branches!  
-      drawBranch();
-      rotate(angle);  
+      branch();
+      pg.rotate(angle);  
     }
-    level--;                                   // once all sub-branches are done
-                                               // return to the lower level to finish
+    level--;                                   
+                                               
   }
-  pop();            // POP back to the previous drawing state. 
+  pg.pop();          
 }
 
 
